ada7d04350ed6eddaf9d8702df90e8f0afbaf246
Sparrow0hawk <alexjcoleman@gmail.com>
2022-11-26 20:06:57 +0000

    create the foo function

---
+++ R/foo.R
@@ -0,0 +1,11 @@
+#' The foo function
+#'
+#' This function foo's it's input argument
+#'
+#' @param x, a value to be foo'd
+#'
+foo <- function(x) {
+    print("Foo")
+    print(x)
+    print("fooFoo")
+}
